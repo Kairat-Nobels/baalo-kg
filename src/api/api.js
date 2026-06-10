a9a04252019459724dcd46805d0b82b320231b07
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2026-06-11 01:40:11 +0600

    done

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,6 @@
-const api = "https://c7f30b7cc3f5dd94.mokky.dev/";
+// const api = "https://c7f30b7cc3f5dd94.mokky.dev/";
+const api = "https://6a29b9aaf59cb8f65f1d8aa5.mockapi.io/";
+
 export const employersApi = api + 'companies'
 export const recordsApi = api + 'records'
 export const reviewsApi = api + 'reviews'
